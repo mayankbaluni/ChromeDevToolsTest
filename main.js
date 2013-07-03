b1adf8412be5db840b87533af61dbd75f533db03
Leonardo Pittelli <leonardo@convoy.me>
2013-07-03 15:59:05 -0300

    agregado save con localstorage

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-window.addEventListener("orientationchange", checkOrientation, false);
+$(window).on("orientationchange", checkOrientation);
 
-$(document).ready(checkOrientation);
+$(document).ready(function(){
+	$('#save').click(saveData);
+	checkOrientation();
+});
 
 function checkOrientation() {
 	if (window.orientation == 0) {
 		$('#orientation').text('Vertical');
 	} else {
 		$('#orientation').text('Horizontal');
+	}
+}
+
+function saveData() {
+	var data = localStorage.getItem("data");
+	if (data != null) {
+		localStorage.setItem("data",parseInt(data)+1);
+	} else {
+		localStorage.setItem("data",0);
 	}
 }
